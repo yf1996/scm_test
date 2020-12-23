0cf48280ded84246a0b95cc0160b372e313aec85
yf1996 <997125133@qq.com>
2020-12-23 17:09:05 +0800

    songyifei

--- test1.c
+++ test1.c
@@ -3,6 +3,23 @@
  * Description: A brief description of this file
  * 
  * Author: Author's name
+ * Date: 2020-12-23 17:07
+ * Last Editors: Editors' name
+ * Last Edit Time: [YYYY-MM-DD HH:mm]
+ * 
+ * Revisions: Rev x.x
+ * History: 
+ * [YYYY-MM-DD]: Description of modification.
+ * 
+ * Copyright(c) 2020 Beijing Institute of Technology.
+ * Lab of Communication and Networking
+ * 
+******************************************************************************/
+/*****************************************************************************/
+/**
+ * Description: A brief description of this file
+ * 
+ * Author: Author's name
  * Date: 2020-12-23 16:45
  * Last Editors: Editors' name
  * Last Edit Time: [YYYY-MM-DD HH:mm]
@@ -32,3 +49,7 @@
  * Lab of Communication and Networking
  * 
 ******************************************************************************/
+int main()
+{
+    return 0;
+}
