c7fd2fbe69d60e274cca5bd64c4a7d58f2cdbba6
yf1996 <997125133@qq.com>
2020-12-23 16:51:02 +0800

    12

---
+++ test1.c
@@ -0,0 +1,34 @@
+/*****************************************************************************/
+/**
+ * Description: A brief description of this file
+ * 
+ * Author: Author's name
+ * Date: 2020-12-23 16:45
+ * Last Editors: Editors' name
+ * Last Edit Time: [YYYY-MM-DD HH:mm]
+ * 
+ * Revisions: Rev x.x
+ * History: 
+ * [YYYY-MM-DD]: Description of modification.
+ * 
+ * Copyright(c) 2020 Beijing Institute of Technology.
+ * Lab of Communication and Networking
+ * 
+******************************************************************************/
+/*****************************************************************************/
+/**
+ * Description: A brief description of this file
+ * 
+ * Author: Author's name
+ * Date: 2020-12-23 16:45
+ * Last Editors: Editors' name
+ * Last Edit Time: [YYYY-MM-DD HH:mm]
+ * 
+ * Revisions: Rev x.x
+ * History: 
+ * [YYYY-MM-DD]: Description of modification.
+ * 
+ * Copyright(c) 2020 Beijing Institute of Technology.
+ * Lab of Communication and Networking
+ * 
+******************************************************************************/
